adbf5dc76d775bbc349527cf16d04eae8169c4fb
arslan7844 <92073881+arslan7844@users.noreply.github.com>
2021-10-06 21:54:01 -0700

    watch and earn

---
+++ myemail.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+  
+<!-- Mirrored from www.paypakearn.com/myemail.php by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 06 Oct 2021 07:38:55 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <title>Forgot Password - Paypak Earn</title>
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <link rel="stylesheet" href="../stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="../use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link type="text/css" rel="stylesheet" href="css/froala_blocks.css">
+    <style>
+      .fdb-block {
+        border-bottom: 1px solid var(--light);
+      }
+    </style>
+  </head>
+  <body style="background-color:#343A40;">
+
+
+
+
+
+ <!--  ---------------------------------Header Nav Menu Start----------------------------------------      --> 
+  
+    <header>
+      <div class="container" style="background-color:#ffffff;">
+        <nav class="navbar navbar-expand-md fixed-top bg-light">
+          <a class="navbar-brand" href="#">
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img src="../paypakearn.com/imgs/paypakearnlogo.png" height="60" alt="image">
+          </a>
+    
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav3" aria-controls="navbarNav3" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+           <div class="collapse navbar-collapse" id="navbarNav3">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link pr-4" href="index-2.html">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item pr-4">
+                <a class="nav-link" href="index.php/index.html#about">About</a>
+              </li>
+              <li class="nav-item pr-4">
+                <a class="nav-link" href="index.php/index.html#plans">Plans</a>
+              </li>
+              <li class="nav-item pr-4">
+                <a class="nav-link" href="index.php/index.html#contact">Contact</a>
+              </li>
+        
+        <li class="nav-item pr-4">
+                <a class="nav-link" href="index.php/index.html#paymentmethods">Payment Methods</a>
+              </li>
+        
+              <li class="nav-item pr-4">
+                <a class="nav-link" href="login.html">Log In</a>
+              </li>
+            </ul>
+    
+            <a class="btn btn-primary ml-md-3" href="register.html">Register</a>
+          </div>
+    </header>
+  
+  
+  
+  <!--  ---------------------------------Header Nav Menu End----------------------------------------      -->
+  
+
+
+
+
+
+
+
+
+<section class="fdb-block" style="background-image: url(imgs/hero/red.svg);">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-8 col-lg-7 col-xl-5 text-center">
+            
+              <form action="https://www.paypakearn.com/sedemail.php" method="post">
+            <div class="fdb-box">
+              <div class="row">
+                
+                <div class="col">
+                  <h4>Email</h4>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col">
+                  <input type="email" class="form-control" placeholder="Enter Your Email" name="myemail" required="required">
+                </div>
+              </div>
+              
+              <div class="row mt-4">
+                <div class="col">
+                  <button class="btn btn-outline-secondary" name="submit" type="submit">Submit</button>
+                 
+                </div>
+              </div>
+            
+             
+             <div id= "error" class="row justify-content-start mt-4" style="color: red; font-weight: 400; font-size: 18px; align-items: center;"> <div>
+            </div>
+          </div>
+        </div>
+        </form>
+      </div>
+    </section>
+	
+	
+	
+
+
+ 
+	
+	
+	
+	
+	
+	<!-- ------------------------------------Footer Start------------------------------------ -->
+	
+	
+	
+
+	
+	<footer class="fdb-block footer-small bg-dark mb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          
+    
+          <div class="col-12 col-md-12 mt-12 mt-md-0 text-center text-md-center">
+            © 2021 Pay Pak Earn. All Rights Reserved
+          </div>
+        </div>
+      </div>
+    </footer>
+	
+	
+	<!-- ------------------------------------Footer End------------------------------------ -->	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</body>
+	
+<!-- Mirrored from www.paypakearn.com/myemail.php by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 06 Oct 2021 07:38:55 GMT -->
+</html>
